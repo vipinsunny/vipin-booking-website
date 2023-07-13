aa044d0dedd6b81631b2c1d65f98b31892b94aa4
vipinsunny <vipinsunny96@gmail.com>
2023-07-13 09:56:15 +0530

    neww

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -3,6 +3,7 @@ import bcrypt from "bcryptjs";
 import { createError } from "../utils/error.js";
 import jwt from "jsonwebtoken";
 
+
 export const register = async (req, res, next) => {
   try {
     const salt = bcrypt.genSaltSync(10);
